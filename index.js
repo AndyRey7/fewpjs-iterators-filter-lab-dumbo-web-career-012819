9027cc0595aaffee6bb9dee3529c0c409834bf72
Andy Reyes <11710reyes@gmail.com>
2019-09-21 17:57:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 const findMatching = (array, drivers) => {
-  return array.filter(driver => driver.toLowerCase() === drivers)
+  return array.filter(driver => driver.toLowerCase() === drivers.toLowerCase())
 }
